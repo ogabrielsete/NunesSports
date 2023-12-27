21083903f4ca4c8ddf7b78bd2afb2e75b8f91e21
Gabriel <biel-sr@hotmail.com>
2023-12-27 17:08:51 -0300

    Add properity ProductCode in Models/Product

--- Models/Product.cs
+++ Models/Product.cs
@@ -8,6 +8,10 @@ public class Product
     [RegularExpression(@"^[\p{L}\s]+$", ErrorMessage = "O nome do pagamento deve conter apenas letras.")]
     public string Name { get; set; }
 
+    [Required(ErrorMessage = "Por favor, insira o código do produto")]
+    [Range(1, 1000000, ErrorMessage = "O código deve ser maior que zero")]
+    public int ProductCode { get; set; }
+
     [Required(ErrorMessage = "Por favor, insira a descrição do produto")]
     [StringLength(250, MinimumLength = 10, ErrorMessage = "A descrição deve conter entre 10 e 100 caracteres.")]
     public string Description { get; set; }
@@ -15,6 +19,6 @@ public class Product
     [Required(ErrorMessage = "Por favor, insira o preço do produto")]
     [DisplayFormat(DataFormatString = "{0:C}")]
     [Range(0.01, 1000000, ErrorMessage = "Valor deve ser maior que zero")]
-    public decimal Price { get; set; }
+    public decimal Price { get; set; } = default;
 
 }
