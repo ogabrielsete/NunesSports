e29cf87575c03895aff80f5eb56da6bf523db807
Gabriel <biel-sr@hotmail.com>
2023-12-27 16:23:40 -0300

    Add Data Annotations to Products Model

--- Models/Product.cs
+++ Models/Product.cs
@@ -5,12 +5,16 @@ public class Product
     public int Id { get; set; }
 
     [Required(ErrorMessage = "Por favor, insira o nome do produto")]
+    [RegularExpression(@"^[\p{L}\s]+$", ErrorMessage = "O nome do pagamento deve conter apenas letras.")]
     public string Name { get; set; }
 
     [Required(ErrorMessage = "Por favor, insira a descrição do produto")]
+    [StringLength(250, MinimumLength = 10, ErrorMessage = "A descrição deve conter entre 10 e 100 caracteres.")]
     public string Description { get; set; }
 
     [Required(ErrorMessage = "Por favor, insira o preço do produto")]
+    [DisplayFormat(DataFormatString = "{0:C}")]
+    [Range(0.01, 1000000, ErrorMessage = "Valor deve ser maior que zero")]
     public decimal Price { get; set; }
 
 }
